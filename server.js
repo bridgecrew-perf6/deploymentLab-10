a6b096ef2c8ec39dec9526f2f105d990a73fcf2d
Tyler Walker <columnedstorm19@gmail.com>
2022-02-24 15:20:54 -0700

    Lab

--- server.js
+++ server.js
@@ -20,22 +20,21 @@ app.post('/api/student', (req, res)=>{
     name = name.trim()
 
     const index = students.findIndex(studentName=> studentName === name)
-
-    if(index === -1 && name !== ''){
+    
+    if(name === 'Tyler'){
+    rollbar.warning("That's my name.")
+    res.status(400).send('Pick a different name, loser')
+    }else if(name === 'Meg'){
+    rollbar.info("That is my partner's name")
+    res.status(400).send('Pick a different name, loser #2')
+    }else if(index === -1 && name !== ''){
         students.push(name)
         rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
         res.status(200).send(students)
     } else if (name === ''){
         rollbar.critical('No name given')
         res.status(400).send('must provide a name.')
-    }else if(name === 'Tyler'){
-        rollbar.warning("That's my name.")
-        res.status(400).send('Pick a different name, loser')
-    }else if(name === 'Meg'){
-        rollbar.info("That is my partner's name")
-        res.status(400).send('Pick a different name, loser #2')
-    } 
-    else {
+    }else {
         rollbar.error('student already exists')
         res.status(400).send('that student already exists')
     }
